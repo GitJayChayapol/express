edc16c338e8476da235f8e96ba4790a3c5c1e2cf
GitJayChayapol <vongjitpimol.jay@hotmail.com>
2023-09-14 11:35:27 +0700

    End Express 02

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const fss = require("fs");
 const fs = require("fs/promises");
 const path = require("path");
 const express = require("express");
@@ -13,6 +14,10 @@ const getDeleted = () => fs.readFile(deletedFile, "utf-8").then(JSON.parse);
 const saveFile = (file, data) =>
   fs.writeFile(file, JSON.stringify(data, null, 2));
 
+if (!fss.existsSync(deletedFile)) {
+  saveFile(deletedFile, []);
+}
+
 const saveToDeleted = (del_item) => {
   getDeleted().then((all_del) => {
     all_del.push(del_item);
@@ -21,12 +26,16 @@ const saveToDeleted = (del_item) => {
 };
 
 app.get("/products/", (req, res) => {
-  const { _page = 1, _limit = 10 } = req.query;
+  const { _page = 1, _limit = 10, _min = 0, _max = 999999 } = req.query;
   getProducts().then((all) => {
+    let productsFilter =
+      _min !== 0 && _max !== 999999
+        ? all.filter((el) => el.price > +_min && el.price < +_max)
+        : all;
     let start = (_page - 1) * _limit;
     let end = start + +_limit;
     let output = all.slice(start, end);
-    res.json({ start, end, output });
+    res.json({ allItem: productsFilter.length, output });
   });
 });
 
@@ -34,11 +43,15 @@ app.delete("/product/:id", (req, res) => {
   const { id } = req.params;
   getProducts().then((all) => {
     let del_idx = all.findIndex((el) => el.id === +id);
+    if (del_idx === -1) return res.status(404).json({ msg: "Data not found" });
     let [del_item] = all.splice(del_idx, 1);
     saveFile(productFile, all);
     saveToDeleted(del_item);
     res.json({ msg: `Deleted id : ${id}` });
   });
+});
+app.use((req, res) => {
+  res.status(404).json({ msg: "Path not found" });
 });
 
 let port = process.env.PORT || 8080;
