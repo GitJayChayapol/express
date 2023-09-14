d90e3a4d3f08355321a9883d525f743dae32a7f4
GitJayChayapol <vongjitpimol.jay@hotmail.com>
2023-09-14 10:35:04 +0700

    Finish Express Homework

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const app = express();
 
 const productFile = path.resolve("db", "products.json");
 // fs.readFile(productFile, "utf-8").then((raw) => JSON.parse(raw)); ตัวไม่ย่อ
+const deletedFile = path.resolve("db", "deleted.json");
 
 const getProducts = () => fs.readFile(productFile, "utf-8").then(JSON.parse);
+const getDeleted = () => fs.readFile(deletedFile, "utf-8").then(JSON.parse);
+const saveFile = (file, data) =>
+  fs.writeFile(file, JSON.stringify(data, null, 2));
 
-app.get("/product/", (req, res) => {
+const saveToDeleted = (del_item) => {
+  getDeleted().then((all_del) => {
+    all_del.push(del_item);
+    return saveFile(deletedFile, all_del);
+  });
+};
+
+app.get("/products/", (req, res) => {
   const { _page = 1, _limit = 10 } = req.query;
   getProducts().then((all) => {
     let start = (_page - 1) * _limit;
@@ -19,5 +30,16 @@ app.get("/product/", (req, res) => {
   });
 });
 
-let port = process.env.PORT || 8000;
+app.delete("/product/:id", (req, res) => {
+  const { id } = req.params;
+  getProducts().then((all) => {
+    let del_idx = all.findIndex((el) => el.id === +id);
+    let [del_item] = all.splice(del_idx, 1);
+    saveFile(productFile, all);
+    saveToDeleted(del_item);
+    res.json({ msg: `Deleted id : ${id}` });
+  });
+});
+
+let port = process.env.PORT || 8080;
 app.listen(port, () => console.log("Server on..", port));
